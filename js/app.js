b1889d28fb0b2337e34421ae907e289481c86607
carina120794@gmail.com <you@example.com>
2017-08-27 22:18:57 -0500

    css

--- js/app.js
+++ js/app.js
@@ -21,6 +21,7 @@ for(var i=0; i<productos.length;i++){
     div_productos.appendChild(div_row);
 }
 
+/*
 div_productos.addEventListener("click",mostrar);
 
 function mostrar(){
@@ -39,3 +40,28 @@ function ocultar(){
 	div_mostrar.style.display="none";
 }
 
+
+*/
+
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById('myImg');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+div_productos.onclick = function(){
+    modal.style.display = "block";
+    var x=event.target.src;
+	modalImg.setAttribute("src",x);
+    /*modalImg.src = this.src;
+    captionText.innerHTML = this.alt;*/
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
